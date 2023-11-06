8dabe98ab8bf131e310faf52b0c456194d3940ae
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-11-06 08:13:15 -0300

    Finish Guessing game with main Rust concepts
    Reference:
     - https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html

--- 3-guessing_game/src/main.rs
+++ 3-guessing_game/src/main.rs
@@ -1,15 +1,41 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100); // ..= means inclusive range
+    let mut guesses_list: Vec<u32> = Vec::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        // If an error occurs, the program will crash and display the message passed to expect.
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
+        let mut guess: String = String::new();
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            // If an error occurs, the program will crash and display the message passed to expect.
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+        guesses_list.push(guess);
+
+        // match is like switch, but more powerful.
+        // It returns a Ordering enum, which has three variants: Less, Greater, and Equal.
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!\n"),
+            Ordering::Greater => println!("Too big!\n"),
+            Ordering::Equal => {
+                println!("You win, with {} guesses!", guesses_list.len());
+                break;
+            }
+        }
+
+        println!("Your guesses: {:?}", guesses_list);
+    }
 }
